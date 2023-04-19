250ca4f5e6cd4a9426319ea72fe27799724dc399
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-04-19 21:02:53 +0900

    remove unused fish functions

--- config.fish/functions/my_custom_function.fish
+++
@@ -1,5 +0,0 @@
-function my_custom_function
-    set word $argv[1]
-    echo "The word under the cursor is: $word"
-end
-
--- config.fish/functions/newline_to_null.fish
+++
@@ -1,5 +0,0 @@
-function newline_to_null
-    while read -l line
-        echo -n -e "$line\0"
-    end
-end
